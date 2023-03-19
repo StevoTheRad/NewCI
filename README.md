4a3583180a2d9a3507690cf55338ce5e12725870
Sergey Petrenko <Stevo98@mail.ru>
2023-03-19 22:15:22 +1000

    update commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/n37irexvpn9emg65/branch/main?svg=true)](https://ci.appveyor.com/project/StevoTheRad/ci/branch/main)
--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -18,7 +18,9 @@ class MobileBankApiTestV4 {
       // Проверки
       .then()
           .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-      ;
+         .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
     }
+
+
 }
